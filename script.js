cb36fc9b6be11105bacce57ed4b83a4c4d943c06
IlyesDjari <ilyes.djari@student.ehb.be>
2021-12-02 13:00:48 +0100

    Adding challenges from database

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 "use strict"
 const baseURL = "http://nxt-level-api.herokuapp.com"
-
+const challengedata = [];
 
 fetch('http://nxt-level-api.herokuapp.com/challenges')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    challengedata.push(data);
+    Object.values(challengedata).forEach(val => {
+      for (let i = 0; i < val.length; i++) {
+        console.log(val[i]);
+        document.getElementById("added").insertAdjacentHTML('beforeend', `
+        <div id="addedchallenge">
+        <h3>${val[i].name}</h3>
+        <p>For ${val[i].course}</p>
+        <p>in session ${val[i].session}</p>
+        <p>On ${val[i].points} points</p>
+        <div class="imgactions">
+            <img src="./images/delete.png" alt="delete" onclick="">
+            <img src="./images/pencil.png" alt="edit" onclick="">
+        </div>
+        </div>`)
+      }
+    })
+  });
 
 
 
